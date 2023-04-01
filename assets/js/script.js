9d36ec84db59079f91fc6359ded014482a73229e
Bruno Gonçalves <bruuno_goncalves@outlook.com.br>
2023-04-01 10:50:44 -0300

    add: game complete

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const scoreText = document.querySelector('.score');
+const resetBtn = document.querySelector('.reset-btn');
+let score = 0;
+
+document.addEventListener('keydown', () => {
+  mario.classList.add('jump');
+
+  setTimeout(() => {
+    mario.classList.remove('jump');
+  },500);
+
+});
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = './assets/images/game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '50px';
+
+    clearInterval(loop);
+  } else if (marioPosition === 0) {
+    score++;
+    scoreText.textContent = `Score: ${score}`;
+  }
+}, 10);
+
+resetBtn.addEventListener('click', () => {
+  location.reload();
+});
